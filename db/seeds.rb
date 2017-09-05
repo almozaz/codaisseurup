747ee86bdc9aabd51130dce023b09d9986bfc50c
almozaz <alacruzmozaz@gmail.com>
2017-09-05 16:18:43 +0200

    Add Event Views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+# ana = User.create!(email: "ana@codaisseurup.com", password: "123456")
+
+# great party", location: "My house", price: 10.5, capacity: 50,
+# includes_food: no, includes_drinks: yes, starts_at: "2017-09-05 12:48:49",ends_at: "2017-09-05 14:48:49",active: yes, user: ana)
